80fcc0f66c84a132baec8bd4976074104e1bcbed
edib <edibersh@gmail.com>
2020-11-25 16:17:48 +0200

    Initial commit

---
+++ app/src/main/java/com/e/mycontentsharing2/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.e.mycontentsharing2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        share_text.setOnClickListener(View.OnClickListener {
+            
+        })
+    }
+}
